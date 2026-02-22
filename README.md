ad0792f799ae12660cac51576f7036644c8bff06
MahdMalik <mahdmahdmalik@gmail.com>
2026-02-21 19:55:10 -0600

    goo goo ga ga

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 hi
+Pain:
+https://borealisdata.ca/dataset.xhtml?persistentId=doi:10.5683/SP3/WCXMAP
+https://osf.io/3hgca/overview?view_only=12b04cd8164d4a6784c04b8c83bf95fb
+
+Stress:
+https://data.mendeley.com/datasets/t93xcwm75r/12
+https://www.kaggle.com/datasets/janithukwattage/stress-faces-dataset 
+
+Emotions:
+https://www.kaggle.com/datasets/deadskull7/fer2013
---
+++ RaspberryPiCode/summarizeImage.py
@@ -0,0 +1,20 @@
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import torch
+
+# Load BLIP processor and model
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# Load your image
+image = Image.open("iStock-490287154_480x480.webp").convert("RGB")
+
+# Prepare inputs
+inputs = processor(images=image, return_tensors="pt")
+
+# Generate caption
+with torch.no_grad():
+    output_ids = model.generate(**inputs, max_length=50)
+caption = processor.decode(output_ids[0], skip_special_tokens=True)
+
+print("Caption:", caption)
